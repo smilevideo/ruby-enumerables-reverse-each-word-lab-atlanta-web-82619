096ceae7021526101af4e47d8b7b7339e05a7735
andrew kim <andrewk1216@gmail.com>
2019-08-21 02:25:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word (str)
   ary = str.split(' ')
-  ary.collect { |word| 
+  new = ary.collect { |word| 
     word.reverse
   }
   
-  ary.join(' ')
-  return ary
+  new.join(' ')
+  return new
 end
